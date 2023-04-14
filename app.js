53754e07b5420c084c37a011f3a38333239af9f4
Erika Prencipe <130106907+3akire@users.noreply.github.com>
2023-04-14 16:20:21 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
 $(document).ready(function(){
-    $('#input').change(function(){
+   
+    $('#in').change(function(){
         var input =$(this).val();
-        $('ul').append(' <li>'+input+'<button class="done">Done</button><button class="canc" >Clear</button></li>');
+        $('ul').append(' <li id=><strong >'+input+'</strong><i class="fa fa-check" ></i> <i class="fa fa-trash-o"></i> <i class="fa fa-pencil-square-o" onclick="cambiaContenuto()"> </i></li>');
         $(this).val('');
     });
 
-    $('ul').on('click','.canc',function(){
+    $('ul').on('click','.fa-trash-o',function(){
         $(this).parent('li').fadeOut(200);
 
     });
 
-    $('ul').on('click','.done',function(){
+    $('ul').on('click','.fa-check',function(){
         $(this).parent('li').toggleClass('checked');
 
     });
 
-    $("button").click(function(){
+    
+    $('ul').on('click','.fa-pencil-square-o',function(){
+        var nuovoContenuto = prompt("Inserisci il nuovo contenuto del paragrafo:");
+        $(this).parent('li').replaceWith(' <li><strong >'+nuovoContenuto+'</strong><i class="fa fa-check" ></i> <i class="fa fa-trash-o"></i> <i class="fa fa-pencil-square-o" onclick="cambiaContenuto()"> </i></li>');   
+        $(this).val('');
+      });
+
+   /* $("button").mouseon(function() {
+        $(this).toggleClass('colored');
+
+    });*/
+    
+
+    $('.all').click(function(){
     $('li').hide(500);
     });
-
 });
